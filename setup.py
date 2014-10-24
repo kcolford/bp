e1f40165285a04002769ed2c424beca176a8eaff
Kieran Colford <colfordk@gmail.com>
2014-10-24 11:04:25 -0400

    improved setup

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
-from distutils import setup
+from distutils.core import setup
 setup(name='Boil',
       version='0.1.1a2',
       description='A configurable boilerplate generator.',
       author='Kieran Colford',
       author_email='colfordk@gmail.com',
+      maintainer='Kieran Colford',
+      maintainer_email='colfordk@gmail.com',
       url='https://github.com/kcolford/bp',
       license='GPLv3',
       packages=['boil'],
