78839939c0cbae47da1821885033d8a8c4e896ca
Kieran Colford <colfordk@gmail.com>
2014-10-24 10:54:21 -0400

    added setup.py script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils import setup
+setup(name='Boil',
+      version='0.1.1a2',
+      description='A configurable boilerplate generator.',
+      author='Kieran Colford',
+      author_email='colfordk@gmail.com',
+      url='https://github.com/kcolford/bp',
+      license='GPLv3',
+      packages=['boil'],
+      scripts=['bp.py'])
+      
