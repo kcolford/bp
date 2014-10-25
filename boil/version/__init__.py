12518b85f5c127655f8ed585c4948ba452bc0f8c
Kieran Colford <colfordk@gmail.com>
2014-10-25 12:23:27 -0400

    version changes progessing

--- boil/version/__init__.py
+++ boil/version/__init__.py
@@ -10,13 +10,20 @@ Currently, only git is a supported VCS.
 
 import re
 import subprocess
-import sys
+import version
 
 def update():
-    """Update this file with a new version string."""
+    """Update the version.py file with a new version string."""
+    
+    try:
+        ver = subprocess.check_output(
+            ['git', 'describe','--abbrev=3', '--always'])
+        ver = ver.split()[0]
+    except OSError, CalledProcessError:
+        return version.version
 
-    # Find ourself in sys.modules
-    filename = sys.modules[__name__].__file__
+    # Get the name of the version file.
+    filename = version.__file__
 
     # Read our code into a string
     with open(filename) as f:
@@ -24,17 +31,13 @@ def update():
 
     # Substitute the default version string with whatever the current
     # one is.
-    code = re.sub(r"^(\s*version = )'.*$", '\g<1>' + repr(version),
+    code = re.sub(r"^(\s*version = )'.*$", '\g<1>' + repr(ver),
                   code, 1, re.M)
 
     # Write the new code back into the file.
     with open(filename, 'w') as f:
         f.write(code)
 
-try:
-    version = subprocess.check_output(['git', 'describe'])
-    version = version.split()[0]
-    update()
-except OSError, CalledProcessError:
-    version = 'v0.1'
+    return ver
 
+version = update()
