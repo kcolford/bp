ff614f16f6529b8d4695bb52aea7043cf7098d7f
Kieran Colford <colfordk@gmail.com>
2014-10-25 13:48:23 -0400

    boil/version.py: perfected the dynamic version string module.

--- boil/version.py
+++ boil/version.py
@@ -2,11 +2,19 @@
 
 Whenever this module is imported, it will always attempt to update its
 version string by fetching a new one from the VCS.  If that fails then
-it retains the one that's already saved to it.
+it retains the one that's already saved to its cache file.  The cache
+file is the current module's _version module (_version.py).
 
 Currently, only git is a supported VCS.
 
 """
+
+# Copyright (C) 2014 Kieran Colford
+
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
 
 import os
 import re
@@ -21,54 +29,62 @@ def getDir():
 
     return thisdir
 
-def makeIgnorefile():
-    """Verify the VCS ignore file is in place.
+def callGit(args):
+    """Call git with args and return the output."""
 
-    This is meant to hide generated _version.py file.
+    return subprocess.check_output(['git', '-C', getDir()] + args,
+                                   stderr=subprocess.STDOUT)
 
-    """
+def acceptsIgnoreRule(path):
+    """Return whether the file at path will accept the ignore rule."""
 
+    with open(path) as f:
+        rules = set(f.readlines())
+    return rules.isdisjoint({'!/_version.py\n', '/_version.py\n'})
+    
+def updateCache(ver):
+    """Update the cache file with the version string ver."""
+
+    # Make sure the ignore file is in place so that our generated file
+    # is ignored by VCS.
     ignorepath = os.path.join(getDir(), '.gitignore')
-    if os.path.exists(ignorepath):
+    if os.path.exists(ignorepath) and acceptsIgnoreRule(ignorepath):
         with open(ignorepath, 'a') as f:
             f.write('/_version.py\n')
     else:
         with open(ignorepath, 'w') as f:
             f.write('/_version.py\n' + '/.gitignore\n')
 
-def updateCache(ver):
-    """Update the cache file with the version string ver."""
-
-    with open(os.path.join(getDir(), '_version.py'), 'w') as f:
+    # Generate the cache module.
+    cachepath = os.path.join(getDir(), '_version.py')
+    with open(cachepath, 'w') as f:
         f.write('version = ' + repr(ver))
 
 def getVersion():
     """Return the version string.
 
     We'll update the _version.py file found in the package if we can.
-    This is where the version string is cached incase the package is
+    This is where the version string is cached in case the package is
     distributed outside of of a git repository.
 
     """
 
     try:
-        import _version
-    except ImportError:
-        # There's no auto generated _version.py file, so we have to
-        # make one.
-        makeIgnorefile()        # Write the .gitignore
-        updateCache('error')    # Initialize the cache file
-        import _version         # Load the cache module
-    
-    try:
-        ver = subprocess.check_output(
-            ['git', 'describe','--abbrev=3', '--always'])
+        ver = callGit(['describe', '--abbrev=3'])
         ver = ver.strip('\n')
+        ver = ver.replace('-', '.', 1)
+        ver = ver.replace('g', '')
         updateCache(ver)
     except OSError, CalledProcessError:
+        import _version
         ver = _version.version
 
     return ver
 
-version = getVersion()
+# Comment/Uncomment this to show/hide the function definitions above.
+# This is useful if the package doesn't want to show the internals of
+# its version generation or if a developer wants to read the module in
+# pydoc.
 __all__ = ['version']
+
+version = getVersion()
