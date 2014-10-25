8fba194dcb5903b3a9b7a419811ed9e51400fad0
Kieran Colford <colfordk@gmail.com>
2014-10-24 20:38:44 -0400

    boil/version.py: improved pydoc appearance

--- boil/version.py
+++ boil/version.py
@@ -12,15 +12,29 @@ import re
 import subprocess
 import sys
 
+def update():
+    """Update this file with a new version string."""
+
+    # Find ourself in sys.modules
+    filename = sys.modules[__name__].__file__
+
+    # Read our code into a string
+    with open(filename) as f:
+        code = f.read()
+
+    # Substitute the default version string with whatever the current
+    # one is.
+    code = re.sub(r"^(\s*version = )'.*$", '\g<1>' + repr(version),
+                  code, 1, re.M)
+
+    # Write the new code back into the file.
+    with open(filename, 'w') as f:
+        f.write(code)
+
 try:
     version = subprocess.check_output(['git', 'describe'])
     version = version.split()[0]
+    update()
 except OSError, CalledProcessError:
-    version = 'v0.0.1'
-    
-filename = sys.modules[__name__].__file__
-with open(filename) as f:
-    code = f.read()
-code = re.sub(r"^(\s*version = )'.*$", '\g<1>' + repr(version), code, 1, re.M)
-with open(filename, 'w') as f:
-    f.write(code)
+    version = 'v0.1'
+
