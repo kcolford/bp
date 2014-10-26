beeaaaebec066d577d064bee3ebc2ccd68afc736
Kieran Colford <colfordk@gmail.com>
2014-10-26 14:07:33 -0400

    now using fixed version to upload msi files
    xdistutils is now a requirement for this

--- upload.sh
+++ upload.sh
@@ -2,5 +2,5 @@
 
 python setup.py register
 python setup.py sdist bdist_egg upload "$@"
-python setup.py bdist_wininst bdist_msi upload "$@"
+python setup.py bdist_wininst bdist_msi_fixed upload "$@"
 python setup.py bdist_rpm upload "$@"
