4e5801ff6e50055a840b3c37ffb76a3310345783
Kieran Colford <colfordk@gmail.com>
2014-11-14 10:39:52 -0500

    added __main__ module

---
+++ txt2boil/__main__.py
@@ -0,0 +1,10 @@
+"""Main routine interface.
+
+The purpose of this is that it is a wrapper that provides the frontend
+ability to invoke this as::
+
+    python -m txt2boil [ARGS...]
+
+"""
+
+from . import main as __main__
