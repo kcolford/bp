6faa3f3a985ac265107a1005a6baa3a65cd5cc99
Kieran Colford <colfordk@gmail.com>
2014-11-14 11:10:58 -0500

    added dynamicgen module

---
+++ txt2boil/dynamicgen.py
@@ -0,0 +1,8 @@
+"""This module contains a dynamic generator class.
+
+It allows on the fly (at runtime) creation of generator classes for
+things such as aliases and custom generators provided in spec files.
+
+"""
+
+from .core import *
