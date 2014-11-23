934440645a384a1f9363e635c206c7247005089b
Kieran Colford <colfordk@gmail.com>
2014-11-23 11:04:22 -0500

    added alias class

--- txt2boil/dynamicgen.py
+++ txt2boil/dynamicgen.py
@@ -6,3 +6,14 @@ things such as aliases and custom generators provided in spec files.
 """
 
 from .core import *
+
+
+class AliasGen(Gen, dict):
+
+    """A generator that provides aliasing syntax."""
+
+    def __missing__(self, key):
+        return None
+    
+    def matchComment(self, comm):
+        pass                    # TODO
